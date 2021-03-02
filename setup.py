ef860129bf289105aa44a1e6a9553157889bf629
tchatzian <75744098+tchatzian@users.noreply.github.com>
2021-03-02 03:27:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import os
-
 from setuptools import setup
-
-this = os.path.dirname(os.path.realpath(__file__))
-
-
-def read(name):
-    with open(os.path.join(this, name)) as f:
-        return f.read()
 
 setup(
         name='psutil-process-monitoring-snap',
@@ -17,7 +8,11 @@ setup(
         author_email='harischatzi@tutanota.com',
         url='https://github.com/tchatzian/psutil-process-monitoring-snap',
         packages=['psutil-process-monitoring-snap'],
-        install_requires=read('requirements.txt'),
         include_package_data=True,
         zip_safe=False,
-)
+        install_requires=[
+            'psutil',
+            'pandas',
+        ],
+        )
+
